ea0cb2c683cb045c86c986cc7982e33ca0023a8a
himathaivalappil <himathaivalappil@gmail.com>
2022-10-26 12:10:37 +0400

    unit test bad parameters added

--- tests/unit/test_yt.py
+++ tests/unit/test_yt.py
@@ -9,8 +9,19 @@ good_URL_data = [
     ("https://www.youtube.com/watch?v=1-68pFs_HIE&list=PLrdaCCBhU_hlLUujFA5KUjb3yathXilut&index=4&t=54s", 54)
 ]
 
+bad_URL_data = [
+    ("https://www.youtube.com/watch?v=twpOdfghfgh0KbJ28M"),
+    ("https://youtu.be/twpO0KbJ28M?&&&t"),
+    ("https://youtu.be/1-68pFs_HIE?tt=-45"),
+    ("https://www.youtube.com/watch?v=1-68pFs_HIE&list=PLrdaCCBhU_hlLUujFA5KUjb3yathXilut&index=4&t==45s"),
+    ("https://www.youtube.com/watch?v=1-68pFs_HIE&list=PLrdaCCBhU_hlLUujFA5KUjb3yathXilut&index=4t=54s")
+]
+
 @pytest.mark.parametrize("URL, response", good_URL_data)
 def test_get_time_info(URL, response):
     assert get_time_info(URL) == response
 
-
+@pytest.mark.parametrize("URL", bad_URL_data)
+def test_get_time_info_failed(URL):
+    with pytest.raises(InvalidURLException):
+        get_time_info(URL)
